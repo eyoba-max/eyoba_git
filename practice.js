133d9796f9e03eef7bc87868904ee2c880e49ee0
moges <eyobaklilu175@gmail.com>
2026-03-08 22:19:33 +0300

    window alert games added like a number gussing game

--- practice.js
+++ practice.js
@@ -12,7 +12,7 @@ document.getElementById("decrease").onclick=function(){
     document.getElementById("lab").textContent=count;
 }
 document.getElementById("increase").onclick=function(){
-    count=2+count;
+    count=++count;
     document.getElementById("lab").textContent=count;
 }
 document.getElementById("reset").onclick=function(){
@@ -47,4 +47,36 @@ else{
     card.textContent=`you didn't select any payment method`;
 }
 
+}
+const minNum=1;
+const maxNum=100;
+const answer=Math.floor(Math.random()*(maxNum-minNum)+minNum);
+
+let attempts=0;
+let guess;
+let running=true;
+
+while(running){
+    guess=window.prompt(`guess a number between ${minNum}-${maxNum}`);
+    guess=Number(guess);
+    if(isNaN(guess)){
+        window.alert("please enter a valid number");
+    }
+    else if(guess<minNum || guess>maxNum){
+        window.alert("please enter a number between the given range");
+    }
+    else{
+       attempts++; 
+       if(guess<answer){
+        window.alert("TOO LOW TRY AGAIN!!");
+       }
+       else if(guess>answer){
+        window.alert("TOO HIGH TRY AGAIN!!");
+       }
+       else{
+        window.alert(`CORRECT! The answer was ${answer} you tried ${attempts} times.`);
+        running=false;
+       }
+    }
+    
 }
