cc7ee78943bee77dae9a280b097210ce7bb039ec
moges <eyobaklilu175@gmail.com>
2026-03-16 12:15:58 +0300

    add function for the game to play

---
+++ rockPaper.js
@@ -0,0 +1,53 @@
+function pickComputerMove(){
+    const randomNum=Math.random();
+    let computerMove='';
+    if(randomNum>=0 && randomNum<1/3){
+        computerMove='rock';
+    }
+    else if(randomNum>=1/3 && randomNum<1/3){
+        computerMove='paper';
+    }
+    else if(randomNum>=2/3 && randomNum<1){
+        computerMove='scissor';
+    }
+    return computerMove;
+}
+
+function playGame(playerMove){
+    const computerMove=pickComputerMove();
+    let result='';
+    if(playerMove==='scissor'){
+        if(computerMove==='rock'){
+            result='you lose';
+        }
+        else if(computerMove==='paper'){
+            result='you win';
+        }
+        else if(computerMove==='scissor'){
+            result='tie';
+        }
+    }
+    if(playerMove==='paper'){
+        if(computerMove==='rock'){
+            result='you win';
+        }
+        else if(computerMove==='paper'){
+            result='tie';
+        }
+        else if(computerMove==='scissor'){
+            result='you lose';
+        }
+    }
+    if(playerMove==='rock'){
+        if(computerMove==='rock'){
+            result='tie';
+        }
+        else if(computerMover==='paper'){
+            result='you lose';
+        }
+        else if(computerMove==='scissor'){
+            result='you win';
+        }
+    }
+alert(`you picked ${playerMove}. Computer picked ${computerMove}. ${result}`);
+}
