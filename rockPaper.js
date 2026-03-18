8cb67874a333e9815a0e9a49c21e13645abb9cf5
moges <eyobaklilu175@gmail.com>
2026-03-18 23:59:30 +0300

    add function for the calculation

--- rockPaper.js
+++ rockPaper.js
@@ -67,3 +67,24 @@ function calculate(){
         display.value="error!!";
     }
 }
+const textBox=document.getElementById("textBox");
+const toFahrenheit=document.getElementById("toFahrenheit");
+const toCcelsius=document.getElementById("toCelsius");
+const result=document.getElementById("result");
+let temp;
+
+function convert(){
+    if(toFahrenheit.checked){
+        temp=Number(textBox.value);
+        temp=temp*9/5+32;
+        result.textContent=temp+"f";
+    }
+    else if(toCelsius){
+        temp=Number(textBox.value);
+        temp=(temp-32)*5/9;
+        result.textContent=temp+"c";
+    }
+    else{
+        result.textContent="slect unit";
+    }
+}
